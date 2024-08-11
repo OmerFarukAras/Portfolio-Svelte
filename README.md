5316cf4cc2e870443fb2e352adcceab979958ed2
omerfarukaras <omerfarukaras62@gmail.com>
2024-08-11 20:55:04 +0300

    License Update

---
+++ README.md
@@ -0,0 +1,15 @@
+# Svelte + Vite + Bootstrap | Portfolio
+This project is licensed under the GNU General Public License v3.0 - see the [LICENSE](./LICENSE) file for details.
+
+## Dev Run
+```shell
+npm install
+npm run dev
+```
+
+## Dist 
+```shell
+npm run build
+```
+
+![Portfolio1](imgs/1.png)![Portfolio1](imgs/2.png)![Portfolio1](imgs/3.png)
--- src/main.js
+++ src/main.js
@@ -1,3 +1,22 @@
+/*
+ * This file is part of Portfolio Svelte.
+ *
+ * Portfolio Svelte is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Portfolio Svelte is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Portfolio Svelte.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Copyright (C) 2024 Omer Faruk Aras
+ */
+
 import './app.css'
 import App from './App.svelte'
 
